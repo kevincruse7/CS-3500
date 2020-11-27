37bad3ce82ff9833f0ae4fa6c1c3e71a1fef5106
kevincruse7 <kevincruse7@gmail.com>
2020-11-27 13:36:37 -0800

    Added Generator class to Assignment 7

---
+++ assignment-7/src/cs3500/animator/generator/Generator.java
@@ -0,0 +1,30 @@
+package cs3500.animator.generator;
+
+import java.io.IOException;
+
+/**
+ * Programmatically generates an Easy Animator input file.
+ */
+public class Generator {
+
+    private final Appendable output;
+
+    /**
+     * Instantiates a {@code Generator} object with the given output appendable.
+     *
+     * @param output Appendable to send output to
+     * @throws NullPointerException Output appendable is null.
+     */
+    public Generator(Appendable output) throws NullPointerException {
+
+    }
+
+    /**
+     * Generates an Easy Animator input file that is sent to the output appendable.
+     *
+     * @throws IOException Output appendable fails.
+     */
+    public void generate() throws IOException {
+
+    }
+}
---
+++ assignment-7/test/cs3500/animator/generator/GeneratorTest.java
@@ -0,0 +1,8 @@
+package cs3500.animator.generator;
+
+/**
+ * Tests the functionality of the {@link Generator} class.
+ */
+public class GeneratorTest {
+
+}
